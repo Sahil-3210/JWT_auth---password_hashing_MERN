cc430084c3a86a8ed250de4b676fab44827d1177
Sahil Randive <randivesahil7@gmail.com>
2024-05-16 19:24:19 +0530

    server and database connection successfull

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
-const express = require("express");
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from "dotenv";
+import cors from "cors";
+
+
+
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
 
-const port = 8000;
+const PORT = process.env.PORT || 5000;
+const URL = process.env.MONGOURL;
+
+mongoose.connect(URL).then(()=>{
+    console.log("Mongoose Connect successfully");
+
+    app.listen(PORT, ()=>{
+        console.log(`server listening on port : ${PORT}`);
+    })
+}).catch(err=> console.log(err));
 
 
-
-app.get("/", (req,res)=>{
-    res.status(201).json("server created")
-})
-
-app.listen(port,()=>{
-    console.log(`server start at port no: ${port}`);
-})
