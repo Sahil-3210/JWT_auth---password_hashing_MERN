3e1ab8cd5e5aeebb1fe99bd619a86592cc7b36b5
Sahil Randive <randivesahil7@gmail.com>
2024-05-16 17:58:51 +0530

    done with login part

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -5,6 +5,42 @@ import "./mix.css";
 const Login = () => {
   const [passShow, setPassShow] = useState(false);
 
+  const [inpval, setInpval] = useState({
+    
+    email:"",
+    password:"",
+    
+  })
+
+  const setVal = (e) => {
+    //console.log(e.target.value);
+    const { name, value } = e.target;
+
+    setInpval(() => {
+      return { ...inpval, [name]: value };
+    });
+  };
+
+  const loginUser = (e)=>{
+    e.preventDefault();
+
+
+    const {email, password} = inpval;
+
+    if (email === "") {
+      alert("please enter your email");
+    } else if (!email.includes("@")) {
+      alert("please enter valid email");
+    } else if (password === "") {
+      alert("please enter your password");
+    } else if (password.length < 6) {
+      alert("password must be atleast 6 character");
+    }else {
+      console.log("success");
+    }
+
+  }
+
   return (
     <section>
       <div className="form_data">
@@ -21,6 +57,8 @@ const Login = () => {
               name="email"
               id="email"
               placeholder="Enter your Email Address"
+              value={inpval.email}
+              onChange={setVal}
             />
           </div>
 
@@ -33,6 +71,8 @@ const Login = () => {
                 name="password"
                 id="password"
                 placeholder="Enter your Password"
+                value={inpval.password}
+                onChange={setVal}
               />
               <div className="showpass" onClick={() => setPassShow(!passShow)}>
                 {!passShow ? "Show" : "Hide"}
@@ -40,7 +80,7 @@ const Login = () => {
             </div>
           </div>
 
-          <button className="btn">Login</button>
+          <button className="btn" onClick={loginUser}>Login</button>
           <p>dont have an account? <NavLink to='/register'>sign up</NavLink></p>
         </form>
       </div>
