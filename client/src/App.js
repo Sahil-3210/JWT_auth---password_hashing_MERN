a65015abb1c64586b1b8ab857cbc3a7734bdd6f0
Sahil Randive <randivesahil7@gmail.com>
2024-05-16 12:28:36 +0530

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
       <Header />
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
       
     </>
   );
