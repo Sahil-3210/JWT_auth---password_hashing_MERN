a72137db773730f58edef4dac89ff6a2d30674d2
Sahil Randive <randivesahil7@gmail.com>
2024-05-18 19:52:20 +0530

    checking if-else and saving the data in db after hashing

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,9 +1,34 @@
 import express from "express"
+import mern from "../models/userSchema.js"
 
 const router = new express.Router();
 
 
 router.post("/register", async(req, res)=>{
-    console.log(req.body);
+    const {fname,email,password,cpassword} = req.body;
+
+    if(!fname|| !email|| !password|| !cpassword){
+        res.status(422).json({error:"please fill all the fields"})
+    }
+
+    try{
+        const preuser = await mern.findOne({email:email})
+        if(preuser){
+            res.status(422).json({error:"user already exist"})
+        }
+        else if(password !== cpassword){
+            res.status(422).json({error:"password and confirm password is not matching."})
+        }
+        else{
+            const finalUser = new mern({fname,email,password,cpassword})
+
+
+            // aftter password hashing in userSchema
+            const storeData = await finalUser.save();
+            console.log(storeData);
+        }
+    }catch(err){
+        res.status(500).json({error:"server error"})
+    }
 })
 export default router;
